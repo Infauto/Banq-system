ed93e294d84f7d4935cd5134d9b765b26b7c2f05
Infauto <40276021+Infauto@users.noreply.github.com>
2018-06-17 22:56:17 +0200

    Create DeclenchementException.java

---
+++ DeclenchementException.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.Object;
+
+public class DeclenchementException
+{
+	public DeclenchementException()
+	{
+		
+	}
+	
+	public DeclenchementException(String definition)
+	{
+		
+	}
+	
+	public static void controle(Scanner sc) throws GestionException
+	{
+		
+	}
+}
